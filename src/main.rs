ec8a4d4aa3904ac982781860aa9af136250f6a3e
JZims <zimmerman.j@hotmail.com>
2023-12-28 14:02:25 -0500

    structs and impl thrown in

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new (name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+     }
+
+     fn greet_visitor(&self) {
+            println!("{}", self.greeting);
+        }
+    
+}
+
+
 
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
@@ -14,21 +34,25 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
+
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert! Enjoy your treehouse."), 
+        Visitor::new("steve", "Hi Steve. Get your own damn milk."), 
+        Visitor::new("fred", "Who let this guy in my treehouse?!"), 
+    
+    ];
+
     println!("Hello, what's your name?");
+    
     let name = what_is_your_name();
-    let mut allow_them_in = false;
-    let visitor_list = ["bert", "steve", "fred", "poop"];
+   
+    let known_visitor = visitor_list
+        .iter()
+        .find(|visitor| visitor.name == name);
 
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allow_them_in = true;
-        }
-    }
-
-    if allow_them_in {
-        println!("Welcome, {}!", name)
-    } else {
-        println!("Sorry, you are not on the list.")
-    }
+   match known_visitor {
+    Some(visitor) => visitor.greet_visitor(),
+    None => println!("You are not on the visitor list. Please leave.")
+   }
     
 }
